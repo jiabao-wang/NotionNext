f0e82b874773e8c375f31a31f38373ba86197675
JiabaoWang <72455983+jiabao-wang@users.noreply.github.com>
2025-09-15 15:52:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.wudibaobaoda.top' },
+    { title: '开心快乐每一天🎉', url: 'https://www.wudibaobaoda.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -20,10 +20,10 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: 'www.wudibaobaoda.top',
+  HEO_HERO_TITLE_4: '无敌宝宝男sp',
+  HEO_HERO_TITLE_5: '专注分享硬核且易懂的编程资源',
+  HEO_HERO_TITLE_LINK: 'https://www.wudibaobaoda.top',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
